7b162e947f21a3d759e313219d45cfb9340d58f0
DaveOC90 <6143678+DaveOC90@users.noreply.github.com>
2021-10-01 15:37:39 -0400

    fixing error in smrToMat

--- smrToMat.py
+++ smrToMat.py
@@ -10,9 +10,9 @@ def smrMatConv(ippath,oppath):
 
     process = subp.Popen(["matlab","-nosplash","-nodesktop","-r","[x]=convertSMRtoMAT('"+ippath+"','"+oppath+"');display(x);exit"], stdout=subp.PIPE, stderr=subp.PIPE)
 
-
-    for line in iter(process.stdout.readline,b''):
-        print('{}'.format(line.rstrip()))
+    stdout, stderr = process.communicate()
+    #for line in iter(process.stdout.readline,b''):
+    #    print('{}'.format(line.rstrip()))
 
     process.kill()
 
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     parser=argparse.ArgumentParser(description='Run script to convert smr files to mat files')
     parser.add_argument('orgDir',type = str, help="Path to organized data directory")
 
-
-    orgdir = parser.orgDir
+    args=parser.parse_args()
+    orgdir = args.orgDir
 
 
     for root, dirs, fs in os.walk(orgdir):
